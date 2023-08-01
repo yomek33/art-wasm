ae4766865c9957a79ef496b26966281fd7c245c0
yomek33 <i3landme@gmail.com>
2023-08-01 18:01:52 +0900

    first

---
+++ ex2/helloworld.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const bytes = fs.readFileSync(__dirname + "/helloworld.wasm");
+
+let hello_world = null;
+let start_string_index = 1000;
+let memory = new WebAssembly.Memory({ initial: 1 });
+let importObject = {
+  env: {
+    buffer: memory,
+    start_string: start_string_index,
+    print_string: function (str_len) {
+      const bytes = new Uint8Array(memory.buffer, start_string_index, str_len);
+      const log_string = new TextDecoder("utf8").decode(bytes);
+      console.log(log_string);
+    },
+  },
+};
+
+(async () => {
+  let obj = await WebAssembly.instantiate(new Uint8Array(bytes), importObject);
+  ({ helloworld: hello_world } = obj.instance.exports);
+  hello_world();
+})();
